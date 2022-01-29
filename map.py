9ef788cb4a09ba05c4aa1edca1454b9081321440
LittleBugEmpire <littlebugempire@gmail.com>
2022-01-29 13:53:48 +0530

    new change

--- map.py
+++ map.py
@@ -17,14 +17,14 @@ html = """<h4>Volcano Information:</h4>
 Name : %s
 """
 
-map = folium.Map(location=[29.48829406821279, 77.69980822497845],zoom_start=5, tiles='Stamen Terrain')
+map = folium.Map(location=[29.48829406821279, 77.69980822497845],zoom_start=5)
 fg = folium.FeatureGroup(name="My Map")
 
 for lt,ln,n in zip(lat, lon, name):
     iframe = folium.IFrame(html=html % n, width = 200, height=100)
-    #fg.add_child(folium.Marker(location=[lt,ln],popup=folium.Popup(iframe),icon=folium.Icon(color=color_producer(n))))
+    fg.add_child(folium.Marker(location=[lt,ln],popup=folium.Popup(iframe),icon=folium.Icon(color=color_producer(n))))
     #custom markers
-    fg.add_child(folium.CircleMarker(location=[lt,ln],radius=6,popup=n,fill_color=color_producer(n),color='grey',fill_opacity=0.7))
+    #fg.add_child(folium.CircleMarker(location=[lt,ln],radius=6,popup=n,fill_color=color_producer(n),color='grey',fill_opacity=0.7))
 
 map.add_child(fg)
 
